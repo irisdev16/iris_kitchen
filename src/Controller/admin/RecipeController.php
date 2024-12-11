0aafe5d0e61ef2944598b51bba40fa548919ff87
Iris <iris.dettori@gmail.com>
2024-12-11 15:14:49 +0100

    ajoute images

--- src/Controller/admin/RecipeController.php
+++ src/Controller/admin/RecipeController.php
@@ -4,8 +4,11 @@ namespace App\Controller\admin;
 
 use App\Entity\Recipe;
 use App\Form\AdminRecipeType;
+use App\Repository\RecipeRepository;
+use App\Repository\TagRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +16,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'recipe')]
-    public function index(): Response
+    #[Route('/recipes', name: 'recipes')]
+    public function index(RecipeRepository $recipeRepository): Response
     {
-        return $this->render('admin/recipe/index.html.twig');
+
+        $recipes = $recipeRepository->findAll();
+        return $this->render('admin/recipe/index.html.twig',[
+            'recipes' => $recipes,
+        ]);
     }
 
 
@@ -25,7 +32,8 @@ class RecipeController extends AbstractController
     //je n'oublie pas de créer ma route avant afin de récupérer les infos d'url comme indiqué ici et récupérer aussi
     // la méthode POST et GET
     #[Route('/admin/recipe/create', name: 'admin_create_recipe', methods: ['GET', 'POST'])]
-    public function adminCreateRecipe(Request $request, EntityManagerInterface $entityManager): Response{
+    public function adminCreateRecipe(Request $request, EntityManagerInterface $entityManager, ParameterBagInterface $parameterBag):
+    Response{
 
         //je créé une variable $recipe qui contient une nouvelle instance de la classe Recipe
         $recipe = new Recipe();
@@ -36,12 +44,32 @@ class RecipeController extends AbstractController
         //j'utilise la méthode handleRequest, instanciéé par symfony
         //le handleRequest récupère les données de POST (donc du form envoyé)
         //pour chaque donné il modifie l'entité (le titre, l'image, etc)
-        //il va remplir l'entité créée par new Recipe avec les données du formulaire remplo
+        //il va remplir l'entité créée par new Recipe avec les données du formulaire rempli
         $adminRecipeForm->handleRequest($request);
         if($adminRecipeForm->isSubmitted()){
-            $this->addFlash('success', 'Recette créée avec success');
+
+            $recipeImage = $adminRecipeForm->get ('image')->getData();
+
+            //s'il a bien une image envoyée
+            //alors je lui donne un identifiant unique, un nom unique pour l'image en gardant l'extension originale(.jpg, .png, etc)
+            //je récupère grace a la classe parameterBag le chemin vers la racine du projet
+            //je génère le chemin vers le dossier images dans le dossier public
+            //je déplace (move) mon image dans le dossier images en la renommant avec l'identifiant unique
+            //j stock dans l'entité le nouveau nom de l'image
+            if($recipeImage){
+
+                $imageNewName = md5(uniqid()).'.'.$recipeImage->guessExtension();
+
+                $rootDir = $parameterBag->get('kernel.project_dir');
+                $imagesDir = $rootDir.'/public//assets/images';
+                $recipeImage->move($imagesDir, $imageNewName);
+
+                $recipe->setImage($imageNewName);
+            }
+
             $entityManager->persist($recipe);
             $entityManager->flush();
+            $this->addFlash('success', 'Recette créée avec success');
 
             return $this->redirectToRoute('admin_create_recipe');
         }
@@ -57,7 +85,29 @@ class RecipeController extends AbstractController
         // variable qui lui est associée
         return $this->render('admin/recipe/create_recipe.html.twig', [
             'adminRecipeFormView' => $adminRecipeFormView,
+            'recipe' => $recipe
         ]);
+    }
 
+    //je créé une méthode qui me permettra d'afficher toutes mes recettes
+    #[Route ('/admin/recipes/list', 'admin_list_recipes',methods: ['GET', 'POST'] )]
+    public function listRecipes(RecipeRepository $recipeRepository): Response{
+
+        $recipes = $recipeRepository->findAll();
+
+    return $this->render('admin/recipe/list_recipes.html.twig', [
+        'recipes' => $recipes,
+    ]);
+
+    }
+
+    #[Route ('recipe/{id}', name: 'recipe_show')]
+    public function show(int $id, RecipeRepository $recipeRepository): Response{
+
+        $recipe = $recipeRepository->find($id);
+
+        return $this->render('admin/recipe/show.html.twig',[
+            'recipe' => $recipe,
+        ]);
     }
 }
--- src/Form/AdminRecipeType.php
+++ src/Form/AdminRecipeType.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use App\Entity\Recipe;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +17,11 @@ class AdminRecipeType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('image')
+            ->add('image', FileType::class, [
+                //ce n'est pas symfony qui utilise le mapping vers les champs de formulaire d'ou mapped=false, c'est pour avoir la main justement sur le mapping
+                'mapped' => false,
+
+            ])
             ->add('ingredients')
             ->add('instructions')
             ->add('category', EntityType::class, [
