7e0ec6831d9ac86bd2f20121fe2aed20ab48ab7b
Iris <iris.dettori@gmail.com>
2024-12-13 12:39:13 +0100

    update des admin + afficher utilisateur connecté dans header

--- src/Controller/admin/AdminUserController.php
+++ src/Controller/admin/AdminUserController.php
@@ -25,10 +25,12 @@ class AdminUserController extends AbstractController
     #[Route('/admin/users/list', name: 'admin_list_users', methods: ['GET'])]
     public function listAdmins(UserRepository $userRepository){
 
+        $authenticatedUser = $this->getUser();
         $admins = $userRepository->findAll();
 
         return $this->render('admin/list_users.html.twig', [
             'admins' => $admins,
+            'authenticatedUser' => $authenticatedUser
         ]);
     }
 
@@ -73,14 +75,63 @@ class AdminUserController extends AbstractController
     #[Route('admin/users/{id}/delete', name: 'admin_delete_user', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function deleteUser(int $id, UserRepository $userRepository, EntityManagerInterface $entityManager){
 
-        $user = $userRepository->find($id);
 
-        $entityManager->remove($user);
-        $entityManager->flush();
+        $userToDelete = $userRepository->find($id);
+        $authenticatedUser = $this->getUser();
+
+        if($id === $authenticatedUser->getId()){
+           $this->addFlash('error', "Vous ne pouvez pas supprimé l'utilisateur connecté");
+
+            return $this->redirectToRoute('admin_list_users');
+        }
+
+
 
         $this->addFlash('success', 'Utilisateur bien supprimé');
 
-        return $this->redirectToRoute('admin_list_users');
+        return $this->redirectToRoute('admin_list_users'[]);
+
+    }
+
+    #[Route('/admin/users/{id}/update', 'admin_update_user', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function updateUser(int $id, UserRepository $userRepository, EntityManagerInterface $entityManager,
+                               UserPasswordHasherInterface $userPasswordHasher, Request $request){
+
+        $user = $userRepository->find($id);
+
+        //dd($user);
+
+        $adminUserForm = $this->createForm(UsersType::class, $user);
+
+        $adminUserForm->handleRequest($request);
+
+        if ($adminUserForm->isSubmitted() && $adminUserForm->isValid()) {
+
+            $password = $adminUserForm->get('password')->getData();
+
+            if($password){
+
+                $hashedPassword = $userPasswordHasher->hashPassword($user, $password);
+
+                $user->setPassword($hashedPassword);
+
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'User créé !!');
+
+            //return $this->redirectToRoute('admin_create_user');
+
+        }
+
+        $adminUserFormView = $adminUserForm->createView();
+
+        return $this->render('admin/update_user.html.twig', [
+            'user' => $user,
+            'adminUserFormView' => $adminUserFormView,
+        ]);
 
     }
 }
